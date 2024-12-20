5805eeca3425dc10334cd5d03d76b0a2a04ed4df
Erik Eiesland <erik.eiesland@nav.no>
2024-12-20 13:40:31 +0100

    hotlibs/katalog -> 24.355.130518

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("no.nav.hjelpemidler:katalog:24.353.162413")
+            from("no.nav.hjelpemidler:katalog:24.355.130518")
         }
     }
 }
